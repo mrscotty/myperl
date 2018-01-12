a58554bee68270a170e610c435ebdf4274985acd
Scott Hardin <scott@hnsc.de>
2018-01-12 20:38:56 +0100

    update debian build

--- debian/build.sh
+++ debian/build.sh
@@ -7,6 +7,9 @@ echo "INFO: ls -latr"
 ls -latr
 
 make debian
+make debian-install
+(cd ex/libdbd-mysql-myperl && make package)
+(cd ex/openxpki-core-deps-myperl && make package)
 
 echo "INFO: find / -name \*.deb"
 find / -name \*.deb
